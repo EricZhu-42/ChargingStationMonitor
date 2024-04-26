21a96782154732d6e2611a1c388c4568bb942895
Null_42 <zhuxinhao00@gmail.com>
2024-04-26 13:06:01 +0800

    Comment out notification

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@ import GroupPanel from './components/GroupPanel.vue';
   <div class="common-layout">
     <el-container>
       <el-header><MyHeader /></el-header>
-      <div style="text-align: center; ">
+      <!-- <div style="text-align: center; ">
         <br/>
         <div style="color:#c45656; font-size:18px">因小程序 API 调整, 充电站状态暂时停止更新，预计在 1~2 天内恢复。</div>
-      </div>
+      </div> -->
       <el-container>
         <el-container>
           <el-main><MyMain /></el-main>
